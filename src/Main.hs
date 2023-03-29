9fa65cdad721ab2731360f8507ee11daba4b09f2
Joshua Niemelä <josh@jniemela.dk>
2023-03-29 23:27:02 +0200

    Made it able to eval stuff

--- src/Main.hs
+++ src/Main.hs
@@ -6,9 +6,11 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Data.Text as T
 import qualified Text.Megaparsec.Char.Lexer as L
+import Control.Monad.Except
 
 -- Define all types for the language
 data SExpr = List [SExpr]
+          | Content [SExpr]
            | Atom Text
            | Number Int
            | String Text
@@ -44,7 +46,7 @@ parseList = List <$> parens (parseSExpr `sepEndBy` space)
 
 parseSExpr :: Parser SExpr
 parseSExpr = do
-    expr <- choice [parseNumber, parseAtom, parseList, parseMarkupBlock]
+    expr <- choice [parseQuoted, parseNumber, parseAtom, parseList, parseMarkupBlock]
     lookAhead $ choice [space, eof]
     -- If the code is list of length 1  then return the first element
     case expr of
@@ -72,22 +74,106 @@ parseMarkup = do
     -- Strip last spaces and newlines
     return $ Markup $ T.pack $ reverse $ dropWhile (\x -> x == ' ' || x == '\n') $ reverse contents
 
+parseQuoted :: Parser SExpr
+parseQuoted = do
+    _ <- char '\''
+    expr <- parseSExpr
+    return $ List [Atom "quote", expr]
+
 
 -- Parsers in code mode
 parseMarkupBlock :: Parser SExpr
 parseMarkupBlock = do
     _ <- char '['
     contents <- manyTill (parseCode <|> parseMarkup) (char ']')
-    return $ List contents
+    return $ Content contents
 
 parseContent :: Parser SExpr
 parseContent = do
     exprs <- manyTill (parseCode <|> parseMarkup) eof
     -- remove empty markup
-    return $ List $ filter (\x -> case x of
+    return $ Content $ filter (\x -> case x of
         Markup "" -> False
         _ -> True) exprs
 
+
+primitives :: [(Text, [SExpr] -> ThrowsError SExpr)]
+primitives = [
+    ("+", numBinop (+)), 
+    ("-", numBinop (-)), 
+    ("*", numBinop (*)),
+    ("/", numBinop div),
+    ("mod", numBinop mod),
+    ("quotient", numBinop quot),
+    ("remainder", numBinop rem)
+    ]
+
+
+eval :: SExpr -> ThrowsError SExpr
+eval val@(String _) = return val
+eval val@(Number _) = return val
+eval val@(Bool _) = return val
+eval val@(Markup _) = return val
+eval (Comment _) = return $ Atom "nil"
+eval (List [Atom "quote", val]) = return val
+eval (List (Atom func : args)) = mapM eval args >>= apply func
+
+-- eval content should return a list of evaluated expressions and concat them to Markup separated by spaces
+eval (Content exprs) = foldl1 (\x y -> do
+    x' <- x
+    y' <- y
+    return $ Markup $ T.concat [markup x', " ", markup y']) $ map eval exprs
+    where
+        markup (Markup x) = x
+        markup x = T.pack $ show x
+
+eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+
+
+apply :: Text -> [SExpr] -> ThrowsError SExpr
+apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" $ show func) ($ args) (lookup func primitives)
+
+
+data Error = NumArgs Int [SExpr]
+           | TypeMismatch String SExpr
+           | BadSpecialForm String SExpr
+           | NotFunction String String
+           | UnboundVar String String
+           | Default String
+
+showError :: Error -> String
+showError (UnboundVar message varname) = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func) = message ++ ": " ++ show func
+showError (NumArgs expected found) = "Expected " ++ show expected
+                                    ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                         ++ ", found " ++ show found
+
+unwordsList :: [SExpr] -> String
+unwordsList = unwords . map show
+
+instance Show Error where show = showError
+
+type ThrowsError = Either Error
+
+trapError action = catchError action (return . show)
+
+extractValue :: ThrowsError a -> a
+extractValue (Right val) = val
+
+
+
+numBinop :: (Int -> Int -> Int) -> [SExpr] -> ThrowsError SExpr
+numBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
+numBinop op params = mapM unpackNum params >>= return . Number . foldl1 op
+numBinop op singleVal@[] = throwError $ NumArgs 2 singleVal
+
+
+unpackNum :: SExpr -> ThrowsError Int
+unpackNum (Number n) = return n
+unpackNum (List [n]) = unpackNum n
+unpackNum notNum = throwError $ TypeMismatch "number" notNum
 
 -- Test random string
 main :: IO ()
@@ -95,4 +181,4 @@ main = do
     content <- readFile "test.phl"
     case parse parseContent "test.phl" (T.pack content) of
         Left err -> putStrLn $ errorBundlePretty err
-        Right expr -> print expr
+        Right expr -> print $ eval expr
